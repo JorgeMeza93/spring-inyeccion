f82d29bbe96e64dc1c312d826d6109916c51d062
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-31 20:38:45 -0600

    Inicio del proyecto

---
+++ src/main/java/com/cursodespring/springboot/web/app/SpringBootInyeccionApplication.java
@@ -0,0 +1,13 @@
+package com.cursodespring.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInyeccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInyeccionApplication.class, args);
+	}
+
+}
